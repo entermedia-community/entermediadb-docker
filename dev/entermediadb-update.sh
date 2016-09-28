eeb9cf8f6a4d041654d82de8ded49e623aa2ebe6
Christopher Burkey <cburkey@openedit.org>
2016-09-28 13:01:16 -0400

    added bin

--- dev/entermediadb-update.sh
+++ dev/entermediadb-update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 wget -O /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
-rm -rf /usr/share/entermediadb/webapp/WEB-INF/{base,lib}
+rm -rf /usr/share/entermediadb/webapp/WEB-INF/{base,lib,bin}
 unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /usr/share/entermediadb/webapp/ > /dev/null
 unzip /tmp/ROOT.war 'WEB-INF/lib/*' -d /usr/share/entermediadb/webapp/ > /dev/null
+unzip /tmp/ROOT.war 'WEB-INF/bin/*' -d /usr/share/entermediadb/webapp/ > /dev/null
+chmod 755 /usr/share/entermediadb/webapp/WEB-INF/bin/linux/*.sh
 rm /tmp/ROOT.war
 /opt/entermediadb/tomcat/bin/shutdown.sh
+
