fdfc87fe9ab92cafcf59c21f1057efd87ef80d81
adam <adam@openedit.org>
2016-10-05 15:32:35 -0400

    update scripts

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ENV CLIENT_NAME=entermedia
 ENV INSTANCE_PORT=8080
 ENV USERID=9009
 ENV GROUPID=9009
-RUN sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers && yum -y install entermediadb_em9
+RUN sed -i -e "s/Defaults    requiretty.*/ #Defaults    requiretty/g" /etc/sudoers && yum -y install unzip entermediadb_em9
 ADD ./entermediadb-deploy.sh /usr/bin/entermediadb-deploy.sh
 ADD ./entermediadb-update.sh /usr/bin/entermediadb-update.sh
 RUN chmod 755 /usr/bin/entermediadb-deploy.sh
--- entermediadb-update.sh
+++ entermediadb-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
+curl -XGET -o /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
 rm -rf /usr/share/entermediadb/webapp/WEB-INF/{base,lib}
 unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /usr/share/entermediadb/webapp/ > /dev/null
 unzip /tmp/ROOT.war 'WEB-INF/lib/*' -d /usr/share/entermediadb/webapp/ > /dev/null
