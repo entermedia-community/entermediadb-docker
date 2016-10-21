3b3101865b79fdc1df50450e1da6823ebca94b64
adam <adam@openedit.org>
2016-10-21 12:01:07 -0400

    missing absolute path

--- entermedia-docker.sh
+++ entermedia-docker.sh
@@ -55,7 +55,7 @@ echo "sudo docker start $INSTANCE" > ${SCRIPTROOT}/start.sh
 echo "sudo docker exec -it $INSTANCE /opt/entermediadb/tomcat/bin/shutdown.sh;sudo docker stop $INSTANCE" > ${SCRIPTROOT}/stop.sh
 echo "sudo docker logs -f --tail 500 $INSTANCE"  > ${SCRIPTROOT}/logs.sh
 echo "sudo docker exec -it $INSTANCE bash"  > ${SCRIPTROOT}/bash.sh
-echo "sudo bash ./entermedia-docker.sh $SITE $PORT" > ${SCRIPTROOT}/update.sh
+echo "sudo bash ${SCRIPTROOT}/entermedia-docker.sh $SITE $PORT" > ${SCRIPTROOT}/update.sh
 echo "sudo docker exec -it -u 0 $INSTANCE entermediadb-update.sh" > ${SCRIPTROOT}/updatedev.sh
 cp  $0  ${SCRIPTROOT}/ 2>/dev/null
 chmod 755 ${SCRIPTROOT}/*.sh
