79e2779e7dd67b6d2c2f6dea7657d82a4210ca65
Christopher Burkey <cburkey@openedit.org>
2016-10-31 22:54:35 -0400

    Simplify

--- entermedia-docker.sh
+++ entermedia-docker.sh
@@ -67,9 +67,9 @@ echo "sudo docker start $INSTANCE" > ${SCRIPTROOT}/start.sh
 echo "sudo docker stop -t 600 $INSTANCE" > ${SCRIPTROOT}/stop.sh
 echo "sudo docker logs -f --tail 500 $INSTANCE"  > ${SCRIPTROOT}/logs.sh
 echo "sudo docker exec -it $INSTANCE bash"  > ${SCRIPTROOT}/bash.sh
-echo "sudo bash ${SCRIPTROOT}/entermedia-docker.sh $SITE $NODENUMBER" > ${SCRIPTROOT}/update.sh
+echo "sudo bash ./entermedia-docker.sh $SITE $NODENUMBER" > ${SCRIPTROOT}/update.sh
 echo "sudo docker exec -it -u 0 $INSTANCE entermediadb-update.sh" > ${SCRIPTROOT}/updatedev.sh
-cp  $0  ${SCRIPTROOT}/ 2>/dev/null
+cp  $0  ${SCRIPTROOT}/entermedia-docker.sh 2>/dev/null
 chmod 755 ${SCRIPTROOT}/*.sh
 
 # Fix permissions
