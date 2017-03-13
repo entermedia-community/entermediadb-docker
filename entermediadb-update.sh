d12c4a9019393e4e1b82a27a98788995b3b0e00b
Christopher Burkey <cburkey@openedit.org>
2017-03-13 14:27:21 -0400

    Upgrade bin

--- entermediadb-update.sh
+++ entermediadb-update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 curl -XGET -o /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
-rm -rf /usr/share/entermediadb/webapp/WEB-INF/{base,lib,bin}
-unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /usr/share/entermediadb/webapp/ > /dev/null
-unzip /tmp/ROOT.war 'WEB-INF/lib/*' -d /usr/share/entermediadb/webapp/ > /dev/null
+rm -rf /opt/entermediadb/webapp/WEB-INF/{base,lib}
+unzip /tmp/ROOT.war 'WEB-INF/lib/*' -d /opt/entermediadb/webapp/ > /dev/null
+unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /opt/entermediadb/webapp/ > /dev/null
+
+##for now
+rm -rf /usr/share/entermediadb/webapp/WEB-INF/{bin}
 unzip /tmp/ROOT.war 'WEB-INF/bin/*' -d /usr/share/entermediadb/webapp/ > /dev/null
 chmod 755 /usr/share/entermediadb/webapp/WEB-INF/bin/linux/*.sh
 rm /tmp/ROOT.war
