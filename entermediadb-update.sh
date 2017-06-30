c1c3866476321c1ca33cbfd1ef998cbbcef71fbb
Christopher Burkey <cburkey@openedit.org>
2017-06-30 13:11:50 -0400

    Smarter upgrade

--- entermediadb-update.sh
+++ entermediadb-update.sh
@@ -1,14 +1,20 @@
 #!/bin/bash 
 curl -XGET -o /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
-rm -rf /opt/entermediadb/webapp/WEB-INF/{base,lib}
-unzip /tmp/ROOT.war 'WEB-INF/lib/*' -d /opt/entermediadb/webapp/ > /dev/null
-unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /opt/entermediadb/webapp/ > /dev/null
+#rm -rf /opt/entermediadb/webapp/WEB-INF/{base,lib}
+rm -rf /tmp/unzip
+mkdir /tmp/unzip
 
-##for now
-rm -rf /usr/share/entermediadb/webapp/WEB-INF/bin
-unzip /tmp/ROOT.war 'WEB-INF/bin/*' -d /usr/share/entermediadb/webapp/ > /dev/null
+unzip /tmp/ROOT.war 'WEB-INF/*' -d /tmp/unzip > /dev/null
+
+rsync -ar --delete /tmp/unzip/lib /opt/entermediadb/webapp/WEB-INF/
+rsync -ar --delete /tmp/unzip/bin /opt/entermediadb/webapp/WEB-INF/
+rsync -ar --delete /tmp/unzip/base /opt/entermediadb/webapp/WEB-INF/
+
+
 chmod 755 /usr/share/entermediadb/webapp/WEB-INF/bin/linux/*.sh
+
 rm /tmp/ROOT.war
+rm -rf /tmp/unzip
 pid=`pgrep -f "entermediadb-deploy.sh"`
 kill -SIGTERM $pid
 echo "Docker restarting"
