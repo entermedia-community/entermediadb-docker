9e5b092c5b7747fbb72d4ca64490ed39a90c155e
Thomas Nadalie <t.nadalie33@gmail.com>
2018-04-27 12:44:45 -0500

    Create client's user and .ssh dir when installing sshd

--- entermediadb-sshd.sh
+++ entermediadb-sshd.sh
@@ -1,6 +1,24 @@
 #!/bin/bash 
+HOME_CLIENT="/home/client"
 
-yum install -y openssh-server git wget
+# Install sshd 
+yum install -y openssh-server
+
+# Generate host ssh keys 
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 ssh-keygen -A
+
+# Generate client SSH key with empty password
+ssh-keygen -t rsa -C "client@entermediadb.org" -f "/media/services/client.pk" -q -N ""
+mv /media/services/client.pk.pub /media/services/client.pub
+
+# Create user client and set primary group to entermedia
+useradd client -g entermedia
+
+#Create .ssh dir with required files
+mkdir $HOME_CLIENT/.ssh
+chmod -v 755 $HOME_CLIENT/.ssh
+cat /media/services/client.pub > $HOME_CLIENT/authorized_keys
+chmod -v 644 $HOME_CLIENT/authorized_keys
+chown -R client:entermedia $HOME_CLIENT/.ssh
