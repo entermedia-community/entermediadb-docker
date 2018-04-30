fd3861d49aa944e59363ab34ab6c9fd2191e28e5
Thomas Nadalie <t.nadalie33@gmail.com>
2018-04-30 12:21:48 -0500

    Call every time and create service and folers once

--- entermediadb-sshd.sh
+++ entermediadb-sshd.sh
@@ -1,36 +1,43 @@
 #!/bin/bash -x
 HOME_CLIENT="/media/services/client"
 
-# Install sshd 
-yum install -y openssh-server wget
-
-# Generate host ssh keys 
-echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-echo -e  'y\n' | ssh-keygen -A
-
-# Generate client SSH key with empty password
+# Create client's home if doesn't exist
 if [ ! -d "$HOME_CLIENT" ]; then
-  mkdir -p $HOME_CLIENT/.ssh
-fi
+	# Install sshd 
+	yum install -y openssh-server wget
+	
+	# Generate host ssh keys 
+	echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+	echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+	echo -e  'y\n' | ssh-keygen -A
 
-if [ ! -f $HOME_CLIENT/.ssh/client.pk ]; then
-    ssh-keygen -t rsa -C "client@entermediadb.org" -f "$HOME_CLIENT/.ssh/client.pk" -q -N ""
-	mv $HOME_CLIENT/.ssh/client.pk.pub $HOME_CLIENT/.ssh/client.pub
+ 	mkdir -p $HOME_CLIENT/.ssh
+ 	chmod -v 755 $HOME_CLIENT/.ssh
+	
+	# Get bash config files from entermedia user
+	cp /home/entermedia/.bash_profile $HOME_CLIENT && cp /home/entermedia/.bashrc $HOME_CLIENT
+
+	# Generate client SSH key with empty password and set authorized_keys
+	if [ ! -f $HOME_CLIENT/.ssh/client.pk ]; then
+	    ssh-keygen -t rsa -C "client@entermediadb.org" -f "$HOME_CLIENT/.ssh/client.pk" -q -N ""
+		mv $HOME_CLIENT/.ssh/client.pk.pub $HOME_CLIENT/.ssh/client.pub
+		cat $HOME_CLIENT/.ssh/client.pub > $HOME_CLIENT/.ssh/authorized_keys
+		chmod -v 644 $HOME_CLIENT/.ssh/authorized_keys
+	fi
 fi
 
 # Create user client and set primary group to entermedia
-useradd client -g entermedia
-# Set client's home
-usermod -m -d $HOME_CLIENT client
+if [ ! id "client" >/dev/null 2>&1 ]; then
+	useradd client -g entermedia -u 777
+	# Set client's home
+	usermod -m -d $HOME_CLIENT client
+fi
 
-#Create .ssh dir with required files
-#mkdir $HOME_CLIENT/.ssh
-chmod -v 755 $HOME_CLIENT/.ssh
-cat $HOME_CLIENT/.ssh/client.pub > $HOME_CLIENT/.ssh/authorized_keys
-chmod -v 644 $HOME_CLIENT/.ssh/authorized_keys
+# Set owner and group for SSH dir
 chown -R client:entermedia $HOME_CLIENT/.ssh
 
 # Get SSH daemon starter script
-wget https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/services/startsshd.sh
-chmod +x /media/service/startsshd.sh
+if [ ! -f /media/service/startsshd.sh ]; then
+	wget https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/services/startsshd.sh
+	chmod +x /media/service/startsshd.sh
+fi
