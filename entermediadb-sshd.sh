6643ba17cbedb88fdf26118bce26ef0106cab649
Thomas Nadalie <t.nadalie33@gmail.com>
2018-04-30 10:54:55 -0500

    Set ssh client home and auto get SSHD starter script

--- entermediadb-sshd.sh
+++ entermediadb-sshd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
-HOME_CLIENT="/home/client"
+HOME_CLIENT="/media/services/client"
 
 # Install sshd 
-yum install -y openssh-server
+yum install -y openssh-server wget
 
 # Generate host ssh keys 
 echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
@@ -10,18 +10,27 @@ echo -e  'y\n' | ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
 echo -e  'y\n' | ssh-keygen -A
 
 # Generate client SSH key with empty password
+if [ ! -d "$HOME_CLIENT" ]; then
+  mkdir -p $HOME_CLIENT/.ssh
+fi
 
-if [ ! -f /media/services/client.pk ]; then
-    ssh-keygen -t rsa -C "client@entermediadb.org" -f "/media/services/client.pk" -q -N ""
-	mv /media/services/client.pk.pub /media/services/client.pub
+if [ ! -f $HOME_CLIENT/.ssh/client.pk ]; then
+    ssh-keygen -t rsa -C "client@entermediadb.org" -f "$HOME_CLIENT/.ssh/client.pk" -q -N ""
+	mv $HOME_CLIENT/.ssh/client.pk.pub $HOME_CLIENT/.ssh/client.pub
 fi
 
 # Create user client and set primary group to entermedia
 useradd client -g entermedia
+# Set client's home
+usermod -m -d $HOME_CLIENT client
 
 #Create .ssh dir with required files
-mkdir $HOME_CLIENT/.ssh
+#mkdir $HOME_CLIENT/.ssh
 chmod -v 755 $HOME_CLIENT/.ssh
-cat /media/services/client.pub > $HOME_CLIENT/.ssh/authorized_keys
+cat $HOME_CLIENT/.ssh/client.pub > $HOME_CLIENT/.ssh/authorized_keys
 chmod -v 644 $HOME_CLIENT/.ssh/authorized_keys
 chown -R client:entermedia $HOME_CLIENT/.ssh
+
+# Get SSH daemon starter script
+wget https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/services/startsshd.sh
+chmod +x /media/service/startsshd.sh
