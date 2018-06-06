7b0e3ea25bb58c54f8739dedd26a943c2ecb4a89
Cristobal <cristobal@entermediasoftware.com>
2018-06-06 14:16:31 -0500

    Firewall with Elastic Support

--- firewall.sh
+++ firewall.sh
@@ -31,9 +31,13 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 #iptables -A INPUT -p icmp -s $SERVER_IP -j ACCEPT
 
 #Finally, drop everyone else
-iptables -A INPUT -j DROP
+iptables -A INPUT -i eth0 -j DROP
 
+#restart docker
+service docker restart
+
+#only allow subnet connections
+iptables -I DOCKER-USER -i eth0 ! -s $LOCAL_NETWORK/24 -j DROP
+
+#save iptables
 iptables-save > /etc/sysconfig/iptables
-
-sudo service docker restart
-
