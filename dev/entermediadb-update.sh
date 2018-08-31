e8f116413c457b4a0b0a2d7f7ae566284107392a
Thomas Nadalie <t.nadalie33@gmail.com>
2018-08-31 13:25:26 -0500

    Add some comments

--- dev/entermediadb-update.sh
+++ dev/entermediadb-update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#Can specify the build number like http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_entermedia-server/2270/artifact/deploy/ROOT.war
+
 wget -O /tmp/ROOT.war http://dev.entermediasoftware.com/jenkins/view/EM9DEV/job/em9dev_demoall/lastSuccessfulBuild/artifact/deploy/ROOT.war > /dev/null
 rm -rf /usr/share/entermediadb/webapp/WEB-INF/{base,lib,bin}
 unzip /tmp/ROOT.war 'WEB-INF/base/*' -d /usr/share/entermediadb/webapp/ > /dev/null
