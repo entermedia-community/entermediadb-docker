fbd7adadd3edb9f3ad7de3867e275fa39c1e84ee
Thomas N <t.nadalie33@gmail.com>
2019-01-09 14:16:32 -0600

    Add more options

--- elastic/health-base.sh
+++ elastic/health-base.sh
@@ -18,6 +18,18 @@ else
 	elif [ $1 == "shards" ]
 	then
 		curl $IP/_cat/shards?v
+	elif [ $1 == "nodes" ]
+	then
+		curl $IP/_nodes
+	elif [ $1 == "master" ]
+	then
+		curl $IP/_cat/master?v
+	elif [ $1 == "masters" ]
+	then
+		curl $IP/_cat/nodes?v&h=id,ip,port,v,m
+   	elif [ $1 == "setmasters" ]
+   	then
+		curl -XPUT $IP/_cluster/settings -d '{ "persistent" : { "discovery.zen.minimum_master_nodes" : '$2'   } }'
 	else
 		echo "Bad argument. Use health.sh [health | nodes | allocation | shards]"
 		exit 1
