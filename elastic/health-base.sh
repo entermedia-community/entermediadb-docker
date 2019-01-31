672f77792d5d6a54a9c183726e4b7f149c1ef293
Thomas N <t.nadalie33@gmail.com>
2019-01-31 16:23:16 -0600

    Add watermark param

--- elastic/health-base.sh
+++ elastic/health-base.sh
@@ -9,7 +9,7 @@ else
 	if [ $1 == "health" ]
 	then
 		curl $IP/_cluster/health?pretty
-	elif [ $1 == "nodes" ]
+	elif [ $1 == "nodes_health" ]
 	then
 		curl $IP/_nodes/_all/?pretty
 	elif [ $1 == "allocation" ]
@@ -27,11 +27,14 @@ else
 	elif [ $1 == "masters" ]
 	then
 		curl $IP/_cat/nodes?v&h=id,ip,port,v,m
-   	elif [ $1 == "setmasters" ]
-   	then
-		curl -XPUT $IP/_cluster/settings -d '{ "persistent" : { "discovery.zen.minimum_master_nodes" : '$2'   } }'
+ 	elif [ $1 == "setmasters" ]
+ 	then
+		curl -XPUT $IP/_cluster/settings -d '{ "persistent" : { "discovery.zen.minimum_master_nodes" : '$2' } }'
+  elif [ $1 == "watermark" ]
+  then
+    curl -XPUT $IP/_cluster/settings -d ' "transient" : { "cluster.routing.allocation.disk.watermark.low" : "5gb","cluster.routing.allocation.disk.watermark.high" : "2gb" } }'
 	else
-		echo "Bad argument. Use health.sh [health | nodes | allocation | shards]"
+		echo "Bad argument. Use health.sh [health | nodes_health | allocation | shards | nodes | master | masters | setmasters | watermark]"
 		exit 1
 	fi
 fi
