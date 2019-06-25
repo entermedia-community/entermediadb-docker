83a3f45fb729bf0576376353b7fc6927fdcb5c3b
Thomas N <t.nadalie33@gmail.com>
2019-06-25 11:25:56 -0500

    Add SSL

--- scripts/setupclient.sh
+++ scripts/setupclient.sh
@@ -5,19 +5,18 @@ if [ "$#" -ne 5 ]; then
     exit 1
 fi
 
-SERVER_ROOT="mediadb08"
 SERVER=$1
 SUBNET=$2
 URL=$3
 NODE=$4
 DNS=$5
 CONFIGFILE="trial-$URL.conf"
-CONFIGFILE_ROOT="trial.redirect-$URL.conf"
-
-FILE_ROOT='server {
-  server_name '$URL'.'$DNS';
-  return 301 http://'$URL'.'$DNS'$request_uri;
-}'
+# CONFIGFILE_ROOT="trial.redirect-$URL.conf"
+#
+# FILE_ROOT='server {
+#   server_name '$URL'.'$DNS';
+#   return 301 http://'$URL'.'$DNS'$request_uri;
+# }'
 
 FILE='server {
   server_name   '$URL'.'$DNS';
@@ -33,12 +32,11 @@ FILE='server {
                     proxy_set_header Host $http_host;
                     proxy_pass http://cluster_'$URL';
   }
-  listen 443 ssl; # managed by Certbot
-  ssl_certificate /etc/letsencrypt/live/'$DNS'/fullchain.pem; # managed by Certbot
-  ssl_certificate_key /etc/letsencrypt/live/'$DNS'/privkey.pem; # managed by Certbot
-  include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
-  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-
+  listen 443 ssl;
+  ssl_certificate /etc/letsencrypt/live/'$DNS'/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/'$DNS'/privkey.pem;
+  include /etc/letsencrypt/options-ssl-nginx.conf;
+  ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 }
 
 upstream cluster_'$URL' {
@@ -46,35 +44,31 @@ upstream cluster_'$URL' {
 }
 
 server {
-    if ($host = '$URL'.'$DNS') {
-        return 301 https://$host$request_uri;
-    } # managed by Certbot
-
   listen        80;
   server_name '$URL'.'$DNS';
-  return 404; # managed by Certbot
+  return 301 https://$host$request_uri;
 }'
-
-deployToOtherServer() {
-	NGINX="sudo mv /home/entermedia/$CONFIGFILE_ROOT /etc/nginx/conf.d && sudo chown root. /etc/nginx/conf.d/$CONFIGFILE_ROOT && sudo nginx -s reload"
-
-	#Create local NGINX config
-	echo -e $FILE_ROOT > /home/entermedia/$CONFIGFILE
-
-	#Deploy NGINX config to server and reload service
-	scp /home/entermedia/$CONFIGFILE_ROOT $SERVER_ROOT:/home/entermedia
-	ssh -tt $SERVER "$NGINX"
-	rm /home/entermedia/$CONFIGFILE
-
-	deployToServer
-}
+#
+# deployToOtherServer() {
+# 	NGINX="sudo mv /home/entermedia/$CONFIGFILE_ROOT /etc/nginx/conf.d && sudo chown root. /etc/nginx/conf.d/$CONFIGFILE_ROOT && sudo nginx -s reload"
+#
+# 	#Create local NGINX config
+# 	echo -e $FILE_ROOT > /home/entermedia/$CONFIGFILE
+#
+# 	#Deploy NGINX config to server and reload service
+# 	scp /home/entermedia/$CONFIGFILE_ROOT $SERVER_ROOT:/home/entermedia
+# 	ssh -tt $SERVER "$NGINX"
+# 	rm /home/entermedia/$CONFIGFILE
+#
+# 	deployToServer
+# }
 
 deployToServer() {
-	DEPLOY="sudo bash ~/entermedia-docker-trial.sh $URL $NODE $SUBNET"
-	NGINX="sudo mv /home/entermedia/$CONFIGFILE /etc/nginx/conf.d && sudo chown root. /etc/nginx/conf.d/$CONFIGFILE && sudo nginx -s reload"
+	DEPLOY="sudo bash /home/entermedia/entermedia-docker-trial.sh $URL $NODE $SUBNET"
+	NGINX="sudo mv /home/entermedia/$CONFIGFILE /etc/nginx/conf.d && sudo chown root. /etc/nginx/conf.d/$CONFIGFILE && sudo chmod 644 /etc/nginx/conf.d/$CONFIGFILE &&sudo nginx -s reload"
 
 	#Deploy docker instance
-	ssh -tt $SERVER "curl -o entermedia-docker-trial.sh -jL https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/entermedia-docker-trial.sh && $DEPLOY"
+	ssh -tt $SERVER "curl -o entermedia-docker-trial.sh -jL docker-trial.entermediadb.org && $DEPLOY"
 
 	#Create local NGINX config
 	echo -e $FILE > /home/entermedia/$CONFIGFILE
@@ -85,8 +79,4 @@ deployToServer() {
 	rm /home/entermedia/$CONFIGFILE
 }
 
-if [ "$SERVER" -ne "$SERVER_ROOT" ]; then
-	deployToOtherServer
-else
-	deployToServer
-fi
+deployToServer
