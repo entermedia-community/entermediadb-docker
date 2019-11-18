aec7352d386c356f191a867493273db97eba1b96
Thomas N <t.nadalie33@gmail.com>
2019-11-18 16:02:52 -0600

    Plugin auto upgrade

--- tomcat/scripts/entermediadb-extensions-deploy.sh
+++ tomcat/scripts/entermediadb-extensions-deploy.sh
@@ -13,6 +13,8 @@ if [ "$(ls -A $DIR)" ]; then
     unzip -o $zip -d $TMP/;
 		if [[ -f $TMP/install.xml ]]; then
 			ant update-dependencies -f $TMP/install.xml;
+			ant unwar -f $TMP/install.xml;
+			ant upgrade -f $TMP/install.xml;
 			ant extend -f $TMP/install.xml;
 		fi
     rm -rf $TMP
