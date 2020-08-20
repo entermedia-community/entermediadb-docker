94c8fc7257a6a1a5f076efc05b78920badd9350b
Cristobal Mejia <cristobal@openedit.org>
2020-08-20 10:40:06 -0500

    Remove Instances

--- scripts/removeclientinstance.sh
+++ scripts/removeclientinstance.sh
@@ -13,6 +13,8 @@ NODE=$3
 DOCKER="sudo docker rm -f $INSTANCE$NODE"
 
 #Delete the EM files under /media/emsite/instance
-ERASE="sudo mv /media/emsites/$INSTANCE /media/emsites/$INSTANCE_old"
+ERASE="sudo mv /media/emsites/$INSTANCE /media/emsites/${INSTANCE}_deleted"
 
-ssh -tt $SERVER "$DOCKER && $ERASE"
+ssh -tt $SERVER "$DOCKER"
+
+ssh -tt $SERVER "$ERASE"
