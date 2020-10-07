f8bfd4d1900bc00e6c3edff049be58cd2bc9106a
Mitsuo Yazawa <mitsuo@entermediadb.org>
2020-10-06 19:41:53 -0600

    Adding timers to un script

--- monitor/un/un.sh
+++ monitor/un/un.sh
@@ -1,11 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 
+if [ "$1" ]; then LOGLEVEL=$1; else LOGLEVEL=0; fi
 
-if [ "$1" ]; then
-  DEBUG=$1
-else
-  DEBUG="false"
-fi
 declare -A globaldns
 globaldns["US1"]="134.209.172.124"
 globaldns["US2"]="165.227.4.225"
@@ -13,32 +9,38 @@ globaldns["EU1"]="206.189.113.193"
 globaldns["EU2"]="159.89.21.156"
 globaldns["AS1"]="139.59.0.94"
 
-
 #globaldns=(${US1} ${US2} ${EU1} ${EU2} ${AS1} )
 
-for SERVER in "${!globaldns[@]}"
-do
+cp -f /etc/hosts /etc/hosts.orig
+start=$(date +%s)
+echo -e "134.209.172.124\tglobal.unitednations.entermediadb.net" >>/etc/hosts # this will add it in case I don't have it
+
+function PRINT() {
+    if [ $LOGLEVEL == 1 ]; then
+        if [ $1 == 1 ]; then echo "DEBUG: $2"; fi
+    fi
+    if [ $LOGLEVEL -ge 1 ]; then
+        if [ $1 == 2 ]; then echo "INFO: $2"; fi
+    fi
+}
+
+for SERVER in "${!globaldns[@]}"; do
     IP=${globaldns[${SERVER}]}
     HOST="global.unitednations.entermediadb.net"
     sed -i "/$HOST/ s/.*/$IP\t$HOST/g" /etc/hosts
-    if [ ${DEBUG} == "true" ]
-    then
-        echo "Verifying: ${SERVER} (${IP})"
-    fi
-    for url in `curl -s https://news.un.org/en/ | sed -En '/<img/s/.*src="(https\:\/\/global\.unitednations\.entermediadb\.net[^"]*)".*/\1/p'`
-    do
-        response=`curl -A "entermediadb-scanner" -s --head --request GET ${url} | grep HTTP/1.1 |  awk {'print $2'};`
-        if [ ${DEBUG} == "true" ]
-        then
-            echo "${response} ${url}"
-            
-        fi
-        if [ ${response} -ne "200" ]
-         then
-#               echo "404 ${url}"
-            echo ""
-        fi
+    PRINT 2 "Verifying: ${SERVER} (${IP})"
+    startip=$(date +%s)
+    for url in $(curl -s https://news.un.org/en/ | sed -En '/<img/s/.*src="(https\:\/\/global\.unitednations\.entermediadb\.net[^"]*)".*/\1/p'); do
+        response=$(curl -A "entermediadb-scanner" -s --head --request GET ${url} | grep HTTP/1.1 | awk {'print $2'})
+        PRINT 1 "${response} ${url}"
+        if [ ${response} -ne "200" ]; then echo "ERROR: failed $HOSTNAME - $URL"; fi
     done
+    endip=$(date +%s)
+    PRINT 2 "$HOST takes: $((endip - startip)) seconds"
 done
 
+end=$(date +%s)
+runtime=$((end - start))
+PRINT 2 "Total Time Verification: $runtime seconds"
 
+cp /etc/hosts.orig /etc/hosts
