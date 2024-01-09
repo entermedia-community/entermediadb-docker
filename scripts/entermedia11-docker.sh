b308b7526f509cc6dd81937f76e4dd52a665ba8e
Cristobal <cristobal.mejia@gmail.com>
2024-01-09 12:27:07 -0600

    11

--- scripts/entermedia11-docker.sh
+++ scripts/entermedia11-docker.sh
@@ -86,7 +86,7 @@ echo "sudo docker logs -f --tail 500 $INSTANCE"  > ${SCRIPTROOT}/logs.sh
 echo "sudo docker exec -it $INSTANCE bash"  > ${SCRIPTROOT}/bash.sh
 echo "sudo bash $SCRIPTROOT/entermedia-docker.sh $SITE $NODENUMBER" > ${SCRIPTROOT}/rebuild.sh
 echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em10.sh $1 $2' > ${SCRIPTROOT}/update-em10.sh
-echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em10.sh $1 $2' > ${SCRIPTROOT}/update-em11.sh
+echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em11.sh $1 $2' > ${SCRIPTROOT}/update-em11.sh
 
 # Health check
 echo "#!/bin/bash +x" > ${SCRIPTROOT}/health.sh
