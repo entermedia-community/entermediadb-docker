ada7423668fdc7c4a6b2045983e01bb9681630c1
Cristobal <cristobal.mejia@gmail.com>
2025-05-29 18:25:13 -0600

    not need any more

--- ubuntu/binaries/face-recognition-decompress.sh
+++
@@ -1,7 +0,0 @@
-
-
-cat ./compare/compare* > compare.tar.gz && tar -xzvf compare.tar.gz;
-cat ./profile/profile* > profile.tar.gz && tar -xvzf profile.tar.gz;
-
-rm profile.tar.gz;
-rm compare.tar.gz;
