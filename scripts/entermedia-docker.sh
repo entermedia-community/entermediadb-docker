f277c8018684741aca443f514f6dca434db3b2e7
Cristobal <cristobal.mejia@gmail.com>
2025-07-24 18:50:53 -0600

    New Deploy Script

--- scripts/entermedia-docker.sh
+++ scripts/entermedia-docker.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 #####################################
 #
-# Launch EnterMediadb 9.x instance
+# Launch EnterMediadb using entermediadb/entermedia:latest Docker image
 #
 #####################################
+
+set -eo pipefail
 
 if [ -z $BASH ]; then
   echo Using Bash...
@@ -14,16 +16,21 @@ fi
 
 # Root check
 if [[ ! $(id -u) -eq 0 ]]; then
-  echo You must run this script as the superuser.
+  echo "You must run this script as the superuser. Usage: sudo ./entermedia-docker.sh sitename nodenumber"
   exit 1
 fi
 
 if [ "$#" -ne 2 ]; then
-    echo "usage: sitename nodenumber"
+    echo "Usage: sudo ./entermedia-docker.sh sitename nodenumber"
     exit 1
 fi
 
 # Setup
+DOCKERPROJECT=entermediadb
+DOCKERIMAGE=entermedia
+BRANCH=latest
+DOCKERNETWORKBASE=172.25.0
+ENDPOINTBASE=/media/emsites
 SITE=$1
 NODENUMBER=$2
 
@@ -31,21 +38,18 @@ if [ ${#NODENUMBER} -ge 4 ]; then echo "Node Number must be between 100-250" ; e
 else echo "Using Node Number: $NODENUMBER"
 fi
 
-
 INSTANCE=$SITE$NODENUMBER
-
-# For dev
-BRANCH=latest
+DOCKERNETWORK=entermedia
 
 # Pull latest images
-docker pull entermediadb/entermediadb9:$BRANCH
+docker pull $DOCKERPROJECT/$DOCKERIMAGE:$BRANCH
 
 ALREADY=$(docker ps -aq --filter name=$INSTANCE)
 [[ $ALREADY ]] && docker stop -t 60 $ALREADY && docker rm -f $ALREADY
 
-IP_ADDR="172.18.0.$NODENUMBER"
+IP_ADDR="$DOCKERNETWORKBASE.$NODENUMBER"
 
-ENDPOINT=/media/emsites/$SITE
+ENDPOINT=$ENDPOINTBASE/$SITE
 
 # Create entermedia user if needed
 if [[ ! $(id -u entermedia 2> /dev/null) ]]; then
@@ -56,26 +60,23 @@ USERID=$(id -u entermedia)
 GROUPID=$(id -g entermedia)
 
 # Docker networking
-if [[ ! $(docker network ls | grep entermedia) ]]; then
-  docker network create --subnet 172.18.0.0/16 entermedia
+if [[ ! $(docker network ls | grep $DOCKERNETWORK) ]]; then
+  docker network create --subnet $DOCKERNETWORKBASE.0/16 $DOCKERNETWORK
 fi
 
 # TODO: support upgrading, start, stop and removing
 
 # Initialize site root
-mkdir -p ${ENDPOINT}/{webapp,data,$NODENUMBER,elastic,services/extensions}
-chown entermedia. ${ENDPOINT}
-chown entermedia. ${ENDPOINT}/{webapp,data,$NODENUMBER,elastic,services}
+if [[ ! -f $ENDPOINT/webapp/index.html ]]; then
+	mkdir -p ${ENDPOINT}/{webapp,data,$NODENUMBER,elastic,services/extensions}
+	chown -R entermedia:entermedia ${ENDPOINT}
+	rm -rf "/tmp/$NODENUMBER"  2>/dev/null
+	mkdir -p "/tmp/$NODENUMBER"
+	chown entermedia:entermedia "/tmp/$NODENUMBER"
+fi
 
 # Create custom scripts
 SCRIPTROOT=${ENDPOINT}/$NODENUMBER
-
-if [ -e $SCRIPTROOT/update.sh ]; then
-    rm $SCRIPTROOT/update.sh
-fi
-if [ -e $SCRIPTROOT/updatedev.sh ]; then
-    rm $SCRIPTROOT/updatedev.sh
-fi
 
 echo "sudo docker start $INSTANCE" > ${SCRIPTROOT}/start.sh
 echo "sudo docker stop -t 60 $INSTANCE" > ${SCRIPTROOT}/stop.sh
@@ -83,9 +84,7 @@ echo "sudo docker stop -t 60 $INSTANCE && sudo docker start $INSTANCE" > ${SCRIP
 echo "sudo docker logs -f --tail 500 $INSTANCE"  > ${SCRIPTROOT}/logs.sh
 echo "sudo docker exec -it $INSTANCE bash"  > ${SCRIPTROOT}/bash.sh
 echo "sudo bash $SCRIPTROOT/entermedia-docker.sh $SITE $NODENUMBER" > ${SCRIPTROOT}/rebuild.sh
-echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update.sh $1 $2' > ${SCRIPTROOT}/update-em9dev.sh
-echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em9.sh $1 $2' > ${SCRIPTROOT}/update-em9.sh
-echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em10.sh $1 $2' > ${SCRIPTROOT}/update-em10dev.sh
+echo 'sudo docker exec -it -u 0 '$INSTANCE' entermediadb-update-em11.sh $1 $2' > ${SCRIPTROOT}/update.sh
 
 # Health check
 echo "#!/bin/bash +x" > ${SCRIPTROOT}/health.sh
@@ -93,37 +92,16 @@ echo "IP=http://$IP_ADDR:9200" >> ${SCRIPTROOT}/health.sh
 wget -O - https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/elastic/health-base.sh >> ${SCRIPTROOT}/health.sh
 
 # Versions
-VERSIONS_FILE=${ENDPOINT}/services/versions.sh
-curl -XGET -o ${ENDPOINT}/services/versions.sh https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/tomcat/services/versions.sh > /dev/null
-chmod +x ${ENDPOINT}/services/versions.sh
-chown entermedia. ${ENDPOINT}/services/versions.sh
-V_DOCKER=$(docker -v | head -n 1 | awk '{print $3}' | sed 's/,//')
-sed -i "s/V_DOCKER_EXT/$V_DOCKER/g;" $VERSIONS_FILE
-#-
+#VERSIONS_FILE=${ENDPOINT}/services/versions.sh
+#curl -XGET -o ${ENDPOINT}/services/versions.sh https://raw.githubusercontent.com/entermedia-community/entermediadb-docker/master/tomcat/services/versions.sh > /dev/null
+#chmod +x ${ENDPOINT}/services/versions.sh
+#chown entermedia. ${ENDPOINT}/services/versions.sh
+#V_DOCKER=$(docker -v | head -n 1 | awk '{print $3}' | sed 's/,//')
+#sed -i "s/V_DOCKER_EXT/$V_DOCKER/g;" $VERSIONS_FILE
+
+
 cp  $0  ${SCRIPTROOT}/entermedia-docker.sh 2>/dev/null
 chmod 755 ${SCRIPTROOT}/*.sh
-
-
-### Fix File Limits in Host Machine
-##if grep -Fq "entermedia" /etc/security/limits.conf
-##then
-##	# code if found
-##	echo ""
-##else
-##	# code if not found
-##	echo "fs.file-max = 10000000" >> /etc/sysctl.conf
-##	echo "entermedia      soft    nofile  409600" >> /etc/security/limits.conf
-##	echo "entermedia      hard    nofile  1024000" >> /etc/security/limits.conf
-##  echo "entermedia      soft    nproc   100000" >> /etc/security/limits.conf
-##  echo "entermedia      hard    nproc   100000" >> /etc/security/limits.conf
-##sysctl -p
-##fi
-
-# Fix permissions
-chown -R entermedia. "${ENDPOINT}/$NODENUMBER"
-rm -rf "/tmp/$NODENUMBER"  2>/dev/null
-mkdir -p "/tmp/$NODENUMBER"
-chown entermedia. "/tmp/$NODENUMBER"
 
 echo "Review the following URL to get the full TZ list"
 echo "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
@@ -133,7 +111,7 @@ set -e
 # Run Create Docker Instance, add Mounted HotFolders as needed
 docker run -t -d \
 	--restart unless-stopped \
-	--net entermedia \
+	--net $DOCKERNETWORK \
 	`#-p 22$NODENUMBER:22` \
 	`#-p 93$NODENUMBER:9300` \
 	`#-p 92$NODENUMBER:9200` \
@@ -152,7 +130,7 @@ docker run -t -d \
 	-v ${ENDPOINT}/services:/media/services \
 	-v ${ENDPOINT}/$NODENUMBER/tmp:/tmp \
     -v ${SCRIPTROOT}/tomcat:/opt/entermediadb/tomcat \
-	entermediadb/entermediadb9:$BRANCH \
+	$DOCKERPROJECT/$DOCKERIMAGE:$BRANCH \
 	/usr/bin/entermediadb-deploy.sh
 
 # Fix /etc/resolv.conf to independently reflect Cloudflare and Google DNS
@@ -166,3 +144,4 @@ docker exec -d $INSTANCE sudo sh -c "echo 'options ndots:0' >>/etc/resolv.conf"
 echo ""
 echo "Node is running: curl http://$IP_ADDR:8080 in $SCRIPTROOT"
 echo ""
+echo "- Run ${SCRIPTROOT}/logs.sh to view logs"
